9099e02d1a08e3d10c95153115c5edd15cd4d093
Imran <Imran@DESKTOP-QQCQFFG>
2022-05-30 17:28:03 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.NewCustomerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OrdersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MetalDepositToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OrderDepositToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomerAccountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuySelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ShopDataToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OthersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DieLossToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MeltLossToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OtherLossToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.MenuStrip1)
+        Me.Panel1.Location = New System.Drawing.Point(1, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(928, 53)
+        Me.Panel1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewCustomerToolStripMenuItem, Me.ToolStripMenuItem1, Me.OrdersToolStripMenuItem, Me.MetalDepositToolStripMenuItem, Me.OrderDepositToolStripMenuItem, Me.CustomerAccountToolStripMenuItem, Me.BuySelToolStripMenuItem, Me.ShopDataToolStripMenuItem, Me.OthersToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(928, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'NewCustomerToolStripMenuItem
+        '
+        Me.NewCustomerToolStripMenuItem.Name = "NewCustomerToolStripMenuItem"
+        Me.NewCustomerToolStripMenuItem.Size = New System.Drawing.Size(98, 20)
+        Me.NewCustomerToolStripMenuItem.Text = "New Customer"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(12, 20)
+        '
+        'OrdersToolStripMenuItem
+        '
+        Me.OrdersToolStripMenuItem.Name = "OrdersToolStripMenuItem"
+        Me.OrdersToolStripMenuItem.Size = New System.Drawing.Size(92, 20)
+        Me.OrdersToolStripMenuItem.Text = "Orders Details"
+        '
+        'MetalDepositToolStripMenuItem
+        '
+        Me.MetalDepositToolStripMenuItem.Name = "MetalDepositToolStripMenuItem"
+        Me.MetalDepositToolStripMenuItem.Size = New System.Drawing.Size(92, 20)
+        Me.MetalDepositToolStripMenuItem.Text = "Metal Deposit"
+        '
+        'OrderDepositToolStripMenuItem
+        '
+        Me.OrderDepositToolStripMenuItem.Name = "OrderDepositToolStripMenuItem"
+        Me.OrderDepositToolStripMenuItem.Size = New System.Drawing.Size(92, 20)
+        Me.OrderDepositToolStripMenuItem.Text = "Order Deposit"
+        '
+        'CustomerAccountToolStripMenuItem
+        '
+        Me.CustomerAccountToolStripMenuItem.Name = "CustomerAccountToolStripMenuItem"
+        Me.CustomerAccountToolStripMenuItem.Size = New System.Drawing.Size(119, 20)
+        Me.CustomerAccountToolStripMenuItem.Text = "Customer Account"
+        '
+        'BuySelToolStripMenuItem
+        '
+        Me.BuySelToolStripMenuItem.Name = "BuySelToolStripMenuItem"
+        Me.BuySelToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.BuySelToolStripMenuItem.Text = "Buy/Sell"
+        '
+        'ShopDataToolStripMenuItem
+        '
+        Me.ShopDataToolStripMenuItem.Name = "ShopDataToolStripMenuItem"
+        Me.ShopDataToolStripMenuItem.Size = New System.Drawing.Size(73, 20)
+        Me.ShopDataToolStripMenuItem.Text = "Shop Data"
+        '
+        'OthersToolStripMenuItem
+        '
+        Me.OthersToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DieLossToolStripMenuItem, Me.MeltLossToolStripMenuItem, Me.OtherLossToolStripMenuItem})
+        Me.OthersToolStripMenuItem.Name = "OthersToolStripMenuItem"
+        Me.OthersToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.OthersToolStripMenuItem.Text = "Others"
+        '
+        'DieLossToolStripMenuItem
+        '
+        Me.DieLossToolStripMenuItem.Name = "DieLossToolStripMenuItem"
+        Me.DieLossToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.DieLossToolStripMenuItem.Text = "Diy Loss"
+        '
+        'MeltLossToolStripMenuItem
+        '
+        Me.MeltLossToolStripMenuItem.Name = "MeltLossToolStripMenuItem"
+        Me.MeltLossToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.MeltLossToolStripMenuItem.Text = "Melt Loss"
+        '
+        'OtherLossToolStripMenuItem
+        '
+        Me.OtherLossToolStripMenuItem.Name = "OtherLossToolStripMenuItem"
+        Me.OtherLossToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.OtherLossToolStripMenuItem.Text = "Other Loss"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(931, 497)
+        Me.Controls.Add(Me.Panel1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents NewCustomerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents OrdersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MetalDepositToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OrderDepositToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomerAccountToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuySelToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShopDataToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OthersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DieLossToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MeltLossToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OtherLossToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs)
+
+    End Sub
+
+    Private Sub OrdersToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OrdersToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub BuySelToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BuySelToolStripMenuItem.Click
+
+    End Sub
+End Class
